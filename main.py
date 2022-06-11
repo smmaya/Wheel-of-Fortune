bce78f2879e11d959c09e63561d74d3dc55da97b
pr0s3q <pr0s3q@gmail.com>
2022-06-11 16:40:03 +0200

    Main while loop

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+
+WIDTH, HEIGHT = 1280, 720
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main():
+    run = True
+    while(run):
+        for event in pygame.event.get():
+            if(event.type == pygame.QUIT):
+                run = False
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()    
